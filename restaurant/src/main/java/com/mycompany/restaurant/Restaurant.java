e603d6d7375f3fa1d4ad2d190174523b02c57c57
MaoJBN <mjbecerran@ufpso.edu.co>
2024-03-15 11:06:59 -0500

    first commit

---
+++ restaurant/src/main/java/com/mycompany/restaurant/Restaurant.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.restaurant;
+
+import food.Breakfast;
+
+public class Restaurant {
+
+    public static void main(String[] args) {
+        Breakfast brfs1 = new Breakfast(2.400,);
+        Breakfast brfs2 = new Breakfast();
+    }
+}
